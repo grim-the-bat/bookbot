dc6b82956bcfcc4844d181b1f27fc9ca7a96c5f1
grim-the-bat <benjaminrgrim@protonmail.com>
2025-03-16 20:23:13 -0400

    Updated with word_count function and changed output to word count.

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def get_book_text(filepath):
  with open(filepath) as file:
     contents = file.read()
     return contents
+
+def word_count(text):
+   text_convert = list(text.split())
+   total_words = len(text_convert)
+   return total_words
+
  
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    total_words = word_count(book_text)
+    print(f"{total_words} words found in the document")
 
 main()
