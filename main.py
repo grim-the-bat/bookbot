43af4f4dc76ee86f5f0bf89101fb749b1248ee09
grim-the-bat <benjaminrgrim@protonmail.com>
2025-03-16 21:58:50 -0400

    Updated stats with sorter; updated with report formatting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, letter_count
+from stats import word_count,dict_sorter 
 
 def get_book_text(filepath):
  with open(filepath) as file:
@@ -6,11 +6,18 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     total_words = word_count(book_text)
-    total_letters = letter_count(book_text)
-    print(f"{total_words} words found in the document")
-    print(total_letters)
-    
+    total_letters = dict_sorter(book_text)
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {book_path}
+----------- Word Count ----------
+Found {total_words} total words
+--------- Character Count -------""")
+    for k,v in total_letters.items():
+       if k.isalpha() == True:
+         print(f"{k}: {v}")  
+    print(f"""============= END ===============""")
 
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,19 @@
 def word_count(text):
-   text_convert = list(text.split())
-   total_words = len(text_convert)
+   text_convert = list(text.split()) #creates list from text on split
+   total_words = len(text_convert) #grabs the len for the total words
    return total_words
 
 def letter_count(text):
-   letters_dict = {}
-   for l in text:
-      lowered = l.lower()
-      if lowered in letters_dict:
-         letters_dict[lowered] += 1
+   letters_dict = {} #blank dictionary
+   for l in text: #for each letter in the text
+      lowered = l.lower() #set it to lower case
+      if lowered in letters_dict: #checks to see if it's in the dicionary
+         letters_dict[lowered] += 1 #if yes, ++
       else:
-         letters_dict[lowered] = 1
-#   sorted_dict = {k: v for k, v in sorted(letters_dict.items(), key=lambda item:item[1], reverse=True)}
+         letters_dict[lowered] = 1 #if not, add value
    return letters_dict
+
+def dict_sorter(text):
+   unsorted_dict = letter_count(text) #grab the letter count dict
+   sorted_dict = {k: v for k, v in sorted(unsorted_dict.items(), key=lambda item:item[1], reverse=True)} #do some kind of magic
+   return sorted_dict
